b09c08f5feb3d631619b0ee9a93868e13f5a5235
KachaKode <142440633+KachaKode@users.noreply.github.com>
2023-10-26 13:00:49 +0000

    Update btree.h

--- src/include/index/btree.h
+++ src/include/index/btree.h
@@ -18,6 +18,8 @@ namespace buzzdb {
 template<typename KeyT, typename ValueT, typename ComparatorT, size_t PageSize>
 struct BTree : public Segment {
     struct Node {
+        // member to hold the parent node 
+        Node *parent; //added 
 
         /// The level in the tree.
         uint16_t level;
@@ -35,8 +37,7 @@ struct BTree : public Segment {
 
     struct InnerNode: public Node {
         /// The capacity of a node.
-        /// TODO think about the capacity that the nodes have.
-        static constexpr uint32_t kCapacity = 42;
+        static constexpr uint32_t kCapacity = (PageSize - sizeof(Node)) / (sizeof(KeyT) + sizeof(ValueT));;
 
         /// The keys.
         KeyT keys[kCapacity];
@@ -50,9 +51,13 @@ struct BTree : public Segment {
         /// Get the index of the first key that is not less than than a provided key.
         /// @param[in] key          The key that should be searched.
         std::pair<uint32_t, bool> lower_bound(const KeyT &key) {
-	      // TODO: remove the below lines of code 
-    	      // and add your implementation here
-            return std::pair<uint32_t, bool>();
+	        for (uint32_t i = 0; i < this->count; ++i) {
+                if (keys[i] >= key) {
+                    return std::make_pair(i, true);  // Found one!
+                }
+            }
+            // Provided key is greater than all keys in the node.
+            return std::make_pair(this->count, false);
         }
 
         /// Insert a key.
@@ -70,9 +75,17 @@ struct BTree : public Segment {
         /// @param[in] buffer       The buffer for the new page.
         /// @return                 The separator key.
         KeyT split(std::byte* buffer) {
-            // TODO: remove the below lines of code 
-    	      // and add your implementation here
-		UNUSED(buffer);
+            // Create a new inner node
+            auto *right_inner_node = new (buffer) InnerNode();
+
+            // Create a new key
+	        uint32_t split_point = this->count / 2;
+            KeyT split_key = keys[split_point - 1];
+
+            right_inner_node->children[0] = children[split_point];
+            right_inner_node->count++;
+            // TO DO: Still more to do...
+	        return split_key;
         }
 
         /// Returns the keys.
@@ -122,6 +135,7 @@ struct BTree : public Segment {
         /// @return                 The separator key.
         KeyT split(std::byte* buffer) {
             // TODO
+            UNUSED(buffer);
         }
 
         /// Returns the keys.
@@ -152,12 +166,65 @@ struct BTree : public Segment {
         next_page_id = 1;
     }
 
+    uint64_t find_leaf_node(const KeyT &key){
+        //  start with the root node 
+        uint64_t current_page_id = root.value_or(0);
+
+        while ( current_page_id != 0 ) {
+            BufferFrame &frame = buffer_manager.fix_page(current_page_id, false);
+            Node *current_node = (Node *) frame.get_data();
+
+            // we've extracted a node, now we need to determine if it's a leaf or not 
+            if (current_node->is_leaf() ) {
+                // found a leaf!  Now we gotta do binary search to find the key 
+                buffer_manager.unfix_page(frame, false);
+                break; 
+            } else {
+                InnerNode *inner_node = (InnerNode *) current_node;
+                auto [index, found] = inner_node->lower_bound(key); 
+                if (found )
+                    current_page_id = inner_node->children[index];
+                else
+                    current_page_id = (index > 0) ? inner_node->children[index-1] : 0 ;
+            }
+        }
+
+        return current_page_id;
+    }
+
     /// Lookup an entry in the tree.
     /// @param[in] key      The key that should be searched.
     std::optional<ValueT> lookup(const KeyT &key) {
-        // TODO
-	UNUSED(key);
-	return std::optional<ValueT>();
+  
+        uint64_t current_page_id = find_leaf_node(key);
+
+        // here leaf should either be found or id is invalid 
+        if (current_page_id != 0){
+            BufferFrame &frame = buffer_manager.fix_page(current_page_id, false) ; 
+            LeafNode *leaf_node = (LeafNode *) frame.get_data() ; 
+
+            // Binary search 
+            int low = 0, high = leaf_node->count - 1;
+            while (low <= high ) {
+                int mid = low + (high - low)/ 2; 
+                if (leaf_node->keys[mid] == key) {
+                    buffer_manager.unfix_page(frame, false);
+                    return leaf_node->values[mid];  
+                }
+                else if (leaf_node->keys[mid] < key){
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+
+            buffer_manager.unfix_page(frame, false);
+        }
+
+        // means that the key wasn't found!!!  
+        return std::optional<ValueT>();
+
     }
 
     /// Erase an entry in the tree.
@@ -184,8 +251,69 @@ struct BTree : public Segment {
 	      next_page_id++;
 	      return;
 	    }
-      UNUSED(key);
-      UNUSED(value);
+
+        uint64_t current_page_id = find_leaf_node(key);
+        Node *parent_node = NULL;
+        uint64_t parent_page_id = 0;
+
+        // here leaf should either be found or id is invalid 
+        if (current_page_id != 0){
+            BufferFrame &leaf_frame = buffer_manager.fix_page(current_page_id, true) ; 
+            LeafNode *leaf_node = (LeafNode *) leaf_frame.get_data() ; 
+
+            KeyT split_key;
+            LeafNode* new_leaf_node = NULL;
+
+            if (leaf_node->count < LeafNode::kCapacity ){
+                leaf_node->insert(key, value);
+            }
+            else {
+                BufferFrame &new_leaf_frame = buffer_manager.fix_page(buffer_manager.get_overall_page_id(segment_id, next_page_id), true) ;
+                new_leaf_node = new ( new_leaf_frame.get_data()) LeafNode();
+                split_key = leaf_node->split((std::byte *)(new_leaf_node));
+                buffer_manager.unfix_page(new_leaf_frame, true);
+                next_page_id++;
+            }
+
+            buffer_manager.unfix_page(leaf_frame, true);
+
+            // now handle the node splits and propagate it up the tree 
+            while (parent_node != NULL && parent_node->count == InnerNode::kCapacity ) {
+                BufferFrame &parent_frame = buffer_manager.fix_page(parent_page_id, true);
+                InnerNode * new_inner_node;
+
+                BufferFrame &new_inner_frame = buffer_manager.fix_page(buffer_manager.get_overall_page_id(segment_id, next_page_id), true) ;
+                new_inner_node = new (new_inner_frame.get_data()) InnerNode();
+                KeyT new_split_key = ((InnerNode *) parent_node))->split(new_inner_node, split_key, new_leaf_node ? next_page_id - 1: 0 ); // What is this line doing?
+
+                // now unfix the new inner and parent frames and then upda new page id var 
+                buffer_manager.unfix_page(new_inner_frame, true);
+                buffer_manager.unfix_page(parent_frame, true);
+                next_page_id++;
+
+                parent_node = parent_node->parent ; 
+                split_key = new_split_key;
+                new_leaf_node = NULL;
+            }
+
+            //  Does the root also need to be split??  
+            if (!parent_node) {
+                BufferFrame &new_root_frame = buffer_manager.fix_page(buffer_manager.get_overall_page_id(segment_id, next_page_id), true);
+                InnerNode *new_root_node = new (new_root_frame.get_data()) InnerNode();
+                new_root_node->keys[0] = split_key;
+                new_root_node->children[0] = root.value_or(0) ;
+                new_root_node->children[1] = next_page_id - 1;
+                new_root_node->count = 1;
+                root = next_page_id ;
+                buffer_manager.unfix_page(new_root_frame, true) ;
+                next_page_id++;
+            }
+        }
+        else {
+            // means that the key wasn't found!!!  
+            return;
+        }
+
     }
 };
 
