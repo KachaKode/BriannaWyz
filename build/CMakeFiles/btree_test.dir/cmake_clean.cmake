2e4da6653152b435a19855a4fde6259e4447ae20
KachaKode <142440633+KachaKode@users.noreply.github.com>
2023-10-27 12:19:05 +0000

    Add files via upload

---
+++ build/CMakeFiles/btree_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/btree_test.dir/test/unit/index/btree_test.cc.o"
+  "CMakeFiles/btree_test.dir/test/unit/index/btree_test.cc.o.d"
+  "test/btree_test"
+  "test/btree_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/btree_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
